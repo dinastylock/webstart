660a241aaca6731199e89eea1c451de287c3ae92
dinastylock <dinastylock778@gmail.com>
2019-10-07 13:20:17 +0300

    модальное оконо переведено на JQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,28 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal')
-var close = document.querySelector('#close')
+// var button = document.querySelector('#button');
+// var modal = document.querySelector('#modal')
+// var close = document.querySelector('#close')
 
-button.addEventListener('click', function () {
-	modal.classList.add('modal_active');
-	let timerId = setTimeout(function () {
-		modal.classList.remove('modal_active');
-	}, 5000);
-});
+// button.addEventListener('click', function () {
+// 	modal.classList.add('modal_active');
+// 	let timerId = setTimeout(function () {
+// 		modal.classList.remove('modal_active');
+// 	}, 5000);
+// });
 
-close.addEventListener('click', function() {
-	modal.classList.remove('modal_active');
+// close.addEventListener('click', function() {
+// 	modal.classList.remove('modal_active');
+// });
+
+
+$(document).ready(function () {
+	var button = $('#button');
+	var modal = $('#modal');
+	var close = $('#close');
+
+	button.on('click', function () {
+		modal.addClass('modal_active');
+	});
+	close.on('click', function () {
+		modal.removeClass('modal_active');
+	});
 });
