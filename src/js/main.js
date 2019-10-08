9e1b8bff6b57a7f76662b5356dc01c2d7c6400af
dinastylock <dinastylock778@gmail.com>
2019-10-08 19:59:05 +0300

    добавлена карусель СЛИК, адаптация

--- src/js/main.js
+++ src/js/main.js
@@ -42,5 +42,29 @@ $(document).ready(function () {
 		});
 	};
 	backToTop();
+
+	$('.slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		prevArrow: $('.arrows__left'),
+		nextArrow: $('.arrows__right'),
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 810,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+		]
+	});
+	
 });
 
